2fadbd2afd3dc5d0c350329262f0b81a2088005e
Andrew Levada <levada.andrew@gmail.com>
2025-03-23 19:17:14 +0300

    look out for links in the sky

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import Image from "next/image";
 import skyImage from "./sky.png";
+import Link from "next/link"
+import { motion } from "framer-motion"
 
 export default function Home() {
   return (
@@ -24,14 +28,68 @@ export default function Home() {
           that help, inspire, and feel nice</p>
         </div>
 
-        <div className="col-span-1 text-center">
+        <div className="col-span-1 text-center space-y-2">
           <p>~ hi, welcome ~</p>
         </div>
 
-        <div className="col-span-1 text-right">
+        <div className="col-span-1 text-right space-y-2">
           <p>2025</p>
+          <p>{floatingLinks.length} links</p>
         </div>
+      </div>
+
+      <div className="absolute inset-0 z-10 mix-blend-color-burn @container">
+        {floatingLinks.map((link) => (
+          <div className="absolute bottom-0 left-1/2 -translate-x-1/2" key={link.href}
+               style={{ transform: `translate(${link.x}px, ${link.y}px)` }}>
+            <motion.div
+              initial={{ y: 0, rotate: link.rotate, color: "#626245" }}
+              whileHover={{ 
+                y: -10,
+                rotate: 0,
+                color: "#000000"
+              }}
+              transition={{ duration: 0.2 }}
+            >
+              <Link href={link.href} target="_blank">
+                <p className="text-center">{link.content}</p>
+              </Link>
+            </motion.div>
+          </div>
+        ))}
       </div>
     </main>
   );
 }
+
+interface FloatingLink {
+  href: string;
+  content: React.ReactNode;
+  x: number;
+  y: number;
+  rotate: number;
+}
+
+const floatingLinks: FloatingLink[] = [
+  {
+    href: "https://portfolio-of-andrew.vercel.app/",
+    content: (<>↗ <br /> portfolio v2 <br /> → Aviasales</>),
+    x: -420,
+    y: -320,
+    rotate: 6
+  },
+  {
+    href: "https://andrewlevada.github.io/",
+    content: (<>↗ <br /> portfolio v1 <br /> → Icons8</>),
+    x: 160,
+    y: -260,
+    rotate: -4
+  },
+  {
+    href: "https://adrw.page/",
+    content: (<>↗ <br /> a pile <br /> of images</>),
+    x: 360,
+    y: -300,
+    rotate: -8
+  }
+]
