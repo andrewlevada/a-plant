3fb7e8a12df3b3ef115029fccb8ec354f2b99c95
Andrew Levada <levada.andrew@gmail.com>
2026-06-03 13:58:55 +0300

    🤷

---
+++ app/perspective/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState, useEffect, useRef } from 'react';
+
+export default function Perspective() {
+    const videoTag = useRef<HTMLVideoElement | null>(null)
+
+    useEffect(() => {
+        navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: true
+        }).then(stream => {
+            if (!videoTag.current) return
+            videoTag.current.srcObject = stream
+        })
+    }, [])
+
+    return <div className="flex flex-col items-center justify-center h-screen">
+      <video
+        className="w-[600px] h-auto top-[300px] left-[300px] bg-[#aaaaaa]"
+        ref={videoTag}
+        autoPlay
+    ></video>
+
+    <canvas
+    ></canvas>
+    </div>;
+  }
