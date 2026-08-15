17007dbbb4749453548c26ca7308203ec7438df6
Andrew Levada <levada.andrew@gmail.com>
2026-08-15 03:38:13 +0300

    added a thesis pdf

--- app/internet-shaper/page.tsx
+++ app/internet-shaper/page.tsx
@@ -1,7 +1,11 @@
-"use client"
-
 export default function InternetShaper() {
-    return <div className="flex flex-col items-center justify-center h-screen">
-        <p>WIP</p>
-    </div>
+    return (
+        <div className="flex items-center justify-center h-screen p-4">
+            <iframe
+                src="/thesis-slides.pdf"
+                title="Thesis slides"
+                className="w-full max-w-[1000px] h-[85vh] border-0"
+            />
+        </div>
+    )
 }
